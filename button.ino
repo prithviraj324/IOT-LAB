8806163cf4fc8589148f890564e3c40f715c1fcb
prithviraj324 <71484638+prithviraj324@users.noreply.github.com>
2021-10-22 15:22:24 +0530

    Add files via upload

---
+++ button.ino
@@ -0,0 +1,19 @@
+int button=2;
+int led=13;
+int buttonState=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(button,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(button);
+  if(buttonState == HIGH) {
+    digitalWrite(led,HIGH);
+  }
+  else if(buttonState == LOW) {
+    digitalWrite(led,LOW);
+  }
+}
