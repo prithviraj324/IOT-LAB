ea37d5970eb7d05fd2c98e2c9adcdef33d7843a6
prithviraj324 <71484638+prithviraj324@users.noreply.github.com>
2021-11-12 15:08:49 +0530

    Add files via upload

---
+++ nightlight.ino
@@ -0,0 +1,21 @@
+int LDRValue=0;
+int lightSensitivity=300;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(11,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  LDRValue=analogRead(A0);
+  Serial.println(LDRValue);
+  delay(50);
+
+  if(LDRValue>lightSensitivity) {
+    digitalWrite(11,LOW);
+  }
+  else {
+    digitalWrite(11,HIGH);
+  }
+}
