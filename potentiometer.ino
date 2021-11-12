b9b5eb7c790da33e6c064df78869f708d481929f
prithviraj324 <71484638+prithviraj324@users.noreply.github.com>
2021-11-12 15:08:25 +0530

    Add files via upload

---
+++ potentiometer.ino
@@ -0,0 +1,16 @@
+int sensorValue=0;
+int outputValue=0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue=analogRead(A0);
+  outputValue=map(sensorValue,0,1023,0,255);
+  analogWrite(9,outputValue);
+  Serial.println(sensorValue);
+  Serial.println(outputValue);
+  delay(100);
+}
