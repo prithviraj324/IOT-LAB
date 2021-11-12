850b573c21f327dabc6680bae6481abd401ccd91
prithviraj324 <71484638+prithviraj324@users.noreply.github.com>
2021-11-12 15:20:44 +0530

    Add files via upload

---
+++ temp_sensor.ino
@@ -0,0 +1,17 @@
+int outputpin=0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int rawVoltage=analogRead(outputpin);
+  float millivolts=(rawVoltage/1024.0)*5000;
+  float celsius=millivolts/10;
+  Serial.print(celsius);
+  Serial.println(" degrees celsius,");
+  Serial.print((celsius*9)/5+32);
+  Serial.println(" degrees fahrenheit,");
+  delay(700);
+}
